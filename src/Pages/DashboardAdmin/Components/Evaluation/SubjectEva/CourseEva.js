c9dc26dc3641319608d67108eb4033f8a0813cc8
Dinith Chanuka <dinithchanuka22@gmail.com>
2019-06-12 12:01:40 +0530

    Lecture Evaluation - Completed

---
+++ src/Pages/DashboardAdmin/Components/Evaluation/SubjectEva/CourseEva.js
@@ -0,0 +1,230 @@
+import React from "react";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon ,MDBCollapse,  MDBDropdownMenu, MDBDropdownItem} from 'mdbreact';
+import Firebase from '../../../../../Components/Firebase/Firebase';
+import Dropdowns from '../../../../../Components/Dropdown/Dropdown';
+import { Button } from 'antd';
+
+import FormExist from './FormExist';
+import FormNew from './FormNew';
+
+class CourseEva extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     code:null,
+     lecid:"",
+     by:"",
+     responsible:"",
+     year:"",
+     option1:"",
+     option2:"",     
+    };
+    this.updateInput = this.updateInput.bind(this)
+    this.addLecEva = this.addLecEva.bind(this)
+  }
+  updateInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+  addLecEva = e => {    
+    e.preventDefault();
+    const db = Firebase.firestore();
+    console.log(db);
+    
+    const userRef = db.collection("evadetails").add({
+      name: this.state.lecid,
+      code: this.state.code,
+      by: this.state.by,
+      responsible: this.state.responsible,
+      year:this.state.year,
+      type: "Course",
+    });
+
+    this.handleForms();
+  }
+  handleForms = () => {
+    var db = Firebase.firestore()
+    var ref = db.collection('evaforms').doc(this.state.code).collection('topics').doc('t-0')
+    var getDoc = ref.get()
+    .then(doc => {
+      if(!doc.exists){
+        console.log('new')
+        this.createNewForm();
+        
+      }else{
+        console.log('old')
+        this.viewExistForm();
+      }
+      console.log(doc);
+    })
+  }
+  viewEvaForm = () => {
+    
+    const collapseID = "basicCollapse";
+    const details = [];
+    var Ref = Firebase.firestore().collection('evaform')
+    var query = Ref.where('code', '==',this.state.code).get()
+    .then(snapshot => {
+      if (snapshot.empty) {
+        this.createForm();
+        console.log(this.state.code);
+        console.log('No matching documents.');
+        return;
+      }
+      snapshot.forEach((doc) =>{
+        const {code,option1,option2} = doc.data();
+        details.push({
+          key:doc.id,
+          code,
+          option1,
+          option2
+        });
+      });
+      this.setState(prevState => ({
+        collapseID: prevState.collapseID !== collapseID ,
+        code:details[0].code,
+      
+      }));
+    })
+    .catch(err => {
+      console.log('Error getting documents', err);
+    });
+    
+  }
+  createNewForm = () => {
+    const collapseID1 = "basicCollapse1";
+    console.log("create new form")
+    this.setState(prevState => ({
+      collapseID1: prevState.collapseID1 !== collapseID1 ,
+    }));
+  }
+  viewExistForm = () => {
+    const collapseID = "basicCollapse";
+    console.log("view exist form")
+
+    this.setState(prevState => ({
+      collapseID: prevState.collapseID !== collapseID ,
+    }));
+  }
+  render(){
+    return (
+      <MDBContainer>
+        <form onSubmit={this.addLecEva}> 
+          <MDBRow>
+            <h4>Lecturer Evaluation</h4>
+          </MDBRow>
+          <MDBRow>
+            <MDBCol md="2">
+              <label
+                htmlFor="defaultFormCardNameEx"
+                className="grey-text font-weight-light"
+              >
+                Code
+              </label>
+              <input
+                type="text"
+                id="defaultFormCardNameEx"
+                className="form-control"
+                name="code"
+                onChange={this.updateInput}
+                value={this.state.code}
+               
+              />
+            </MDBCol>
+            <MDBCol md="2">
+              <label
+                htmlFor="defaultFormCardNameEx"
+                className="grey-text font-weight-light"
+              >
+                Year
+              </label>
+              <input
+                type="text"
+                id="defaultFormCardNameEx"
+                className="form-control"
+                name="year"
+                onChange={this.updateInput}
+                value={this.state.year}
+               
+              />
+            </MDBCol>
+            <MDBCol md="2">
+            <label
+                htmlFor="defaultFormCardNameEx"
+                className="grey-text font-weight-light"
+              >
+                Course Code
+            </label>
+              <Dropdowns 
+                className="form-control" 
+                name="lecid" 
+                default="(Please select the course)"
+                onChange={this.updateInput}
+                value={this.state.course}
+                dbName="lecturers"
+                fieldName="id">
+              </Dropdowns>
+            </MDBCol>
+            <MDBCol md="3">
+            <label
+                htmlFor="defaultFormCardNameEx"
+                className="grey-text font-weight-light"
+              >
+                Evaluated By
+            </label>
+            <Dropdowns
+                className="form-control" 
+                name="by" 
+                default="(Please select the student group)"
+                onChange={this.updateInput}
+                value={this.state.by}
+                dbName="groups"
+                fieldName="code">
+            </Dropdowns>
+            </MDBCol>
+            <MDBCol md="3">
+            <label
+                htmlFor="defaultFormCardNameEx"
+                className="grey-text font-weight-light"
+              >
+                Responsible
+            </label>
+            <Dropdowns 
+                className="form-control" 
+                name="responsible" 
+                default="(Please select the student group)"
+                onChange={this.updateInput}
+                value={this.state.responsible}
+                dbName="lecturers"
+                fieldName="id">
+            </Dropdowns>
+            </MDBCol>
+          </MDBRow>
+          
+          <div className="text-center py-4 mt-3">
+            <MDBBtn className="btn btn-outline-purple" type="submit">
+              Confirm
+            <MDBIcon far icon="paper-plane" className="ml-2" />
+            </MDBBtn>
+            <hr></hr>
+          </div>
+        </form>
+
+        
+         
+        <MDBCollapse id="basicCollapse" isOpen={this.state.collapseID}>
+          <FormExist evacode={this.state.code}></FormExist>
+        </MDBCollapse>
+
+        <MDBCollapse id="basicCollapse1" isOpen={this.state.collapseID1}>
+          <FormNew evacode={this.state.code}></FormNew>
+        </MDBCollapse>
+                 
+
+      </MDBContainer>
+    );  
+  };
+};
+export default CourseEva;
+
---
+++ src/Pages/DashboardAdmin/Components/Evaluation/SubjectEva/FormExist.js
@@ -0,0 +1,144 @@
+import React from "react";
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon ,MDBCollapse,  MDBDropdownMenu, MDBDropdownItem} from 'mdbreact';
+import Firebase from '../../../../../Components/Firebase/Firebase'
+import Dropdowns from '../../../../../Components/Dropdown/Dropdown';
+import { Button } from 'antd';
+
+import Topic from './Topic'
+
+class FormExist extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+         topics: [],
+        };
+    }
+
+    // componentDidMount() {
+    //   setTimeout(() => { this.getDetails()}, 5000);
+      
+    // }
+    componentDidUpdate(){
+      this.getDetails()
+    }
+    
+    getDetails = () => {
+      const details = [];
+      var code = this.evacode.value
+      
+      if(code != ""){
+        var evaformsRef = Firebase.firestore().collection('evaforms').doc(code).collection('topics')
+        evaformsRef.get().then(collections => {
+          console.log('oooooo')
+          console.log(this.props.evacode)
+          console.log('col', collections);
+          const newTopics = [];
+          collections.forEach(collection => {
+            newTopics.push({
+              name: collection.data().name,
+              criterias: collection.data().criterias
+            });
+          });
+          console.log('new topics', newTopics);
+          this.setState({ topics: newTopics });
+        })
+      }
+      
+    }
+    addTopic = () => {
+      const topics = this.state.topics.slice();
+      const newTopics = topics.concat([{ 
+        num: `t-${topics.length}`,
+        name: "", 
+        criterias: [] 
+      }]);
+
+      this.setState({ topics: newTopics});
+    };
+
+    removeTopic = idx => () => {
+      this.setState({
+        topics: this.state.topics.filter((t, tidx) => idx !== tidx)
+      });
+    };
+
+    addCriteria = idx => () => {
+      const topics = this.state.topics.slice();
+      topics[idx].criterias = topics[idx].criterias.concat({
+        num: `c-${idx}-${topics[idx].criterias.length}`,
+        criteria: '', 
+        forNow:'', 
+        method: ''
+      });
+      this.setState({topics})
+    }
+
+    removeCriteria = topicIdx => critIdx => () => {
+      const topics = this.state.topics.slice();
+      const criterias = topics[topicIdx].criterias;
+      topics[topicIdx].criterias = criterias.filter((t, idx) => {
+        return idx !== critIdx;
+      })
+      this.setState({topics});
+    }
+
+    handleTopicNameChange = idx => (e) => {
+      const topics = this.state.topics.slice();
+      topics[idx].name = e.target.value;
+      this.setState({topics});
+    }
+
+    handleCriteriaChange = topicIdx => critIdx => fieldName => e => {
+      const topics = this.state.topics.slice();
+      topics[topicIdx].criterias[critIdx][fieldName] = e.target.value;
+      this.setState({topics});
+    }
+
+    handleSubmit = (e) => {
+      e.preventDefault();
+      const db = Firebase.firestore();
+
+      this.state.topics.forEach(topic => {
+        const r = db.collection('evaforms')
+          .doc(this.props.evacode)
+          .collection('topics')
+          .doc(topic.num)
+          .set(topic);
+      });
+    }
+
+    render(){
+        return(
+          <form onSubmit={this.handleSubmit}>
+          <h5 color="red">Form exists...</h5>
+          <div>
+            {this.state.topics.map((topic, idx) => (
+              <Topic 
+                key={'topic-'+idx} 
+                topic={topic}
+                topicNameOnChange={this.handleTopicNameChange(idx)}
+                onDelete={this.removeTopic(idx)}
+                toAddCriteria={this.addCriteria(idx)}
+                toDeleteCriteria={this.removeCriteria(idx)}
+                onCriteriaChange={this.handleCriteriaChange(idx)}
+                criterias={this.state.topics[idx].criterias}
+              ></Topic>
+            ))}
+            <input type="hidden" name="id"  value={this.props.evacode} ref={(input) => { this.evacode = input }}/>
+            <MDBRow>
+              <MDBCol md = "3">
+                <Button type="primary" onClick={this.addTopic} >Add New Topic</Button>
+              </MDBCol>
+            </MDBRow>  
+          </div>
+          <div className="text-center py-4 mt-3">
+            <MDBBtn className="btn btn-outline-purple" type="submit">
+              Submit
+            <MDBIcon far icon="paper-plane" className="ml-2" />
+            </MDBBtn>
+          </div>
+        </form>
+        )
+    }
+};
+export default FormExist;
